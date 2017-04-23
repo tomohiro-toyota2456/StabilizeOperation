88142c19c9dbc4dfd787bdea81fe48cfeb44be47
harada2 <createdirect2@gmail.com>
2017-04-23 16:36:16 +0900

    FOV変更機能追加

---
+++ Assets/SceneData/Game/Script/FovSlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+public class FovSlider : MonoBehaviour
+{
+  [SerializeField]
+  Camera camera;
+  [SerializeField]
+  Slider slider;
+
+	// Use this for initialization
+	void Start ()
+  {
+    slider.value = slider.maxValue / 2;
+
+    slider.OnValueChangedAsObservable()
+      .Subscribe(value =>
+      {
+        camera.fieldOfView = value;
+      }).AddTo(gameObject);
+	}
+	
+
+}
