42c55248b9dc0d5cf9a256a27fe1c9af79c6bbda
harada2 <createdirect2@gmail.com>
2017-04-24 00:17:37 +0900

    カメラ移動スクリプト追加　押しっぱなしを検知するスクリプトの追加

---
+++ Assets/SceneData/Common/Script/ObservableDownKeepTrigger.cs
@@ -0,0 +1,55 @@
+﻿namespace Common
+{
+  using System.Collections;
+  using System.Collections.Generic;
+  using UnityEngine;
+  using UniRx;
+  using UniRx.Triggers;
+  using UnityEngine.EventSystems;
+  using System;
+
+  public class ObservableDownKeepTrigger : ObservableTriggerBase,IPointerDownHandler,IPointerUpHandler
+  {
+    float? raiseTime = null;
+    Subject<float> pointerDownKeepObserver;
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(raiseTime != null)
+      {
+        if(pointerDownKeepObserver != null)
+        {
+          float time = Time.realtimeSinceStartup - raiseTime.Value;
+          pointerDownKeepObserver.OnNext(time);
+        }
+      }
+    }
+
+    protected override void RaiseOnCompletedOnDestroy()
+    {
+
+    }
+
+    public void OnPointerDown(PointerEventData _eventData)
+    {
+      raiseTime = Time.realtimeSinceStartup;
+    }
+
+    public void OnPointerUp(PointerEventData _eventData)
+    {
+      raiseTime = null;
+    }
+
+    public IObservable<float> OnDownKeepObservable()
+    {
+      return pointerDownKeepObserver == null ? pointerDownKeepObserver = new Subject<float>() : pointerDownKeepObserver;
+    }
+
+    private void OnDisable()
+    {
+      raiseTime = null;
+    }
+
+  }
+}
---
+++ Assets/SceneData/Game/Script/CameraMoveButton.cs
@@ -0,0 +1,88 @@
+﻿namespace Game
+{
+  using System.Collections;
+  using System.Collections.Generic;
+  using UnityEngine;
+  using UnityEngine.UI;
+  using UniRx;
+  using Common;
+
+
+  public class CameraMoveButton : MonoBehaviour
+  {
+    [SerializeField]
+    float moveVal;
+    [SerializeField]
+    Camera camera;
+    [SerializeField]
+    Button upButton;
+    [SerializeField]
+    Button downButton;
+    [SerializeField]
+    Button leftButton;
+    [SerializeField]
+    Button rightButton;
+
+
+    // Use this for initialization
+    void Start()
+    {
+      upButton.gameObject.AddComponent<ObservableDownKeepTrigger>()
+        .OnDownKeepObservable()
+        .Subscribe(_=>
+        {
+          MoveCameraUp();
+        }).AddTo(gameObject);
+
+      downButton.gameObject.AddComponent<ObservableDownKeepTrigger>()
+        .OnDownKeepObservable()
+        .Subscribe(_ =>
+        {
+          MoveCameraDown();
+        }).AddTo(gameObject);
+
+      leftButton.gameObject.AddComponent<ObservableDownKeepTrigger>()
+        .OnDownKeepObservable()
+        .Subscribe(_ =>
+        {
+          MoveCameraLeft();
+        }).AddTo(gameObject);
+
+      rightButton.gameObject.AddComponent<ObservableDownKeepTrigger>()
+        .OnDownKeepObservable()
+        .Subscribe(_ =>
+        {
+          MoveCameraRight();
+        }).AddTo(gameObject);
+    }
+
+    public void MoveCameraLeft()
+    {
+      var pos = camera.transform.localPosition;
+      pos.x += -moveVal;
+      camera.transform.localPosition = pos;
+    }
+
+    public void MoveCameraRight()
+    {
+      var pos = camera.transform.localPosition;
+      pos.x += moveVal;
+      camera.transform.localPosition = pos;
+
+    }
+
+    public void MoveCameraUp()
+    {
+      var pos = camera.transform.localPosition;
+      pos.z += moveVal;
+      camera.transform.localPosition = pos;
+    }
+
+    public void MoveCameraDown()
+    {
+      var pos = camera.transform.localPosition;
+      pos.z += -moveVal;
+      camera.transform.localPosition = pos;
+    }
+  }
+}
